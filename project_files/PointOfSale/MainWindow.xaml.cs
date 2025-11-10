6e8d7e275c46ea35d2133118859a37028e757753
DaPakoREE <dapakoreeee@gmail.com>
2025-11-10 14:45:37 +0100

    Slightly more complete design

--- project_files/PointOfSale/MainWindow.xaml.cs
+++ project_files/PointOfSale/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace PointOfSale
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
