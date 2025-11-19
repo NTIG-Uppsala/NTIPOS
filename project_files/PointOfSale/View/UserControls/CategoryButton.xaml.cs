95a9958a26cc9352fa92da4c42064f9c8c3de82d
DaPakoREE <dapakoreeee@gmail.com>
2025-11-19 15:53:23 +0100

    Added non functional category buttons

---
+++ project_files/PointOfSale/View/UserControls/CategoryButton.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CategoryButton.xaml
+    /// </summary>
+    public partial class CategoryButton : UserControl, INotifyPropertyChanged
+    {
+        public CategoryButton()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string categoryName;
+
+        public string CategoryName
+        {
+            get { return categoryName; }
+            set { 
+                categoryName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Brush categoryColor;
+
+        public Brush CategoryColor
+        {
+            get { return categoryColor; }
+            set { 
+                categoryColor = value;
+                OnPropertyChanged("CategoryColor");
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
