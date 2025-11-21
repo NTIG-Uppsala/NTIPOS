1e8f1f1dd13cf37be694984e027a0bbbc69b2505
DaPakoREE <dapakoreeee@gmail.com>
2025-11-21 11:52:16 +0100

    Alot of changes
    First off we changed everything so that we now use the MVVM architectural pattern. After that, we removed our ProductCategory class and made one singular list for all products. We then fixed up our Products.xaml by removing our use of UserControls and instead implemented it directly into the xaml to fix our ItemsControl not working. Lastly, we added a ScrollViewer to the ItemsControl so that if we add more products, you will still be able to see all of them by using the scroll bar

--- project_files/PointOfSale/View/UserControls/Products.xaml.cs
+++ project_files/PointOfSale/View/UserControls/Products.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PointOfSale.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -21,86 +23,11 @@ namespace PointOfSale.View.UserControls
     /// Interaction logic for Products.xaml
     /// </summary>
     public partial class Products : UserControl
-
     {
-        public static Products ProductsClass { get; set; }
         public Products()
         {
-            ProductsClass = this;
-
-            /// Products List
-            List<ProductCategory> categoryList =
-            [
-                new ProductCategory("Tobak", "tobakProducts", Brushes.SaddleBrown,      [new Product("Marlboro Red (20-pack)", "Tobak", 89),
-                                                                                         new Product("Camel Blue (20-pack)", "Tobak", 85),
-                                                                                         new Product("L&M Filter (20-pack)", "Tobak", 79),
-                                                                                         new Product("Skruf Original Portion", "Tobak", 62),
-                                                                                         new Product("Göteborgs Rapé White Portion", "Tobak", 67),
-                ]),
-
-                new ProductCategory("Godis", "godisProducts", Brushes.LightPink,        [new Product("Marabou Mjölkchoklad 100 g", "Godis", 25),
-                                                                                         new Product("Daim dubbel", "Godis", 15),
-                                                                                         new Product("Kexchoklad", "Godis", 12),
-                                                                                         new Product("Malaco Gott & Blandat 160 g", "Godis", 28),
-                ]),
-
-                new ProductCategory("Enkel mat", "matProducts", Brushes.Orange,         [new Product("Korv med bröd", "Enkel mat", 89),
-                                                                                         new Product("Varm toast (ost & skinka)", "Enkel mat", 30),
-                                                                                         new Product("Pirog (köttfärs)", "Enkel mat", 22),
-                                                                                         new Product("Färdig sallad (kyckling)", "Enkel mat", 49),
-                                                                                         new Product("Panini (mozzarella & pesto)", "Enkel mat", 45),
-                ]),
-
-                new ProductCategory("Tidningar", "tidningProducts", Brushes.LightBlue,  [new Product("Aftonbladet (dagens)", "Tidningar", 28),
-                                                                                         new Product("Expressen (dagens)", "Tidningar", 28),
-                                                                                         new Product("Illustrerad Vetenskap", "Tidningar", 79),
-                                                                                         new Product("Kalle Anka & Co", "Tidningar", 45),
-                                                                                         new Product("Allt om Mat", "Tidningar", 69),
-                ]),
-            ];
-
             InitializeComponent();
-            TJOSAN.ItemsSource = categoryList;
-        }
-
-        public class ProductCategory
-        {
-            public string Name
-            { get; set; }
-
-            public string Key
-            { get; set; }
-
-            public Brush Color
-            { get; set; }
-
-            public List<Product> ProductsList
-            { get; set; }
-
-            public ProductCategory(string categoryName, string categoryKey, Brush categoryColor, List<Product> categoryProductsList)
-            {
-                Name = categoryName;
-                Key = categoryKey;
-                Color = categoryColor;
-                ProductsList = categoryProductsList;
-            }
-        }
-
-        public class Product
-        {
-            public string Name
-            { get; set; }
-            public string Category
-            { get; set; }
-            public int Price
-            { get; set; }
-
-            public Product(string productName, string productCategory, int priceInt)
-            {
-                Name = productName;
-                Category = productCategory;
-                Price = priceInt;
-            }
+            DataContext = new ProductsViewModel();
         }
     }
 }
---
+++ project_files/PointOfSale/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using PointOfSale.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PointOfSale.ViewModel
+{
+    public class ProductsViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ProductsViewModel()
+        {
+            Products = new ObservableCollection<Product>
+            {
+                new Product("Marlboro Red (20-pack)", "Tobak", 89),
+                new Product("Camel Blue (20-pack)", "Tobak", 85),
+                new Product("L&M Filter (20-pack)", "Tobak", 79),
+                new Product("Skruf Original Portion", "Tobak", 62),
+                new Product("Göteborgs Rapé White Portion", "Tobak", 67),
+
+                new Product("Marabou Mjölkchoklad 100 g", "Godis", 25),
+                new Product("Daim dubbel", "Godis", 15),
+                new Product("Kexchoklad", "Godis", 12),
+                new Product("Malaco Gott & Blandat 160 g", "Godis", 28),
+
+                new Product("Korv med bröd", "Enkel mat", 89),
+                new Product("Varm toast (ost & skinka)", "Enkel mat", 30),
+                new Product("Pirog (köttfärs)", "Enkel mat", 22),
+                new Product("Färdig sallad (kyckling)", "Enkel mat", 49),
+                new Product("Panini (mozzarella & pesto)", "Enkel mat", 45),
+
+                new Product("Aftonbladet (dagens)", "Tidningar", 28),
+                new Product("Expressen (dagens)", "Tidningar", 28),
+                new Product("Illustrerad Vetenskap", "Tidningar", 79),
+                new Product("Kalle Anka & Co", "Tidningar", 45),
+                new Product("Allt om Mat", "Tidningar", 69),
+            };
+        }
+    }
+}
